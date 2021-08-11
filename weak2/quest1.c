65b094f706a1783e9a856e449b63db74439b9386
sanjaynegi681 <74060688+sanjaynegi681@users.noreply.github.com>
2021-08-11 22:37:58 +0530

    Update quest1.c

--- weak2/quest1.c
+++ weak2/quest1.c
@@ -1,1 +1,30 @@
-
+#include<stdio.h>
+void main()
+{
+	int i,j,k,n,arr[100],key,count=0;
+	scanf("%d",&n);
+	for(j=0;j<n;j++)
+	{
+		scanf("%d",&k);
+		for(i=0;i<k;i++)
+		{
+			scanf("%d",&arr[i]);
+		}
+		scanf("%d",&key);
+		for(i=0;i<k;i++)
+		{
+			if(key==arr[i])
+			{
+				count++;
+			}
+		}
+		if(count>=1)
+		{
+			printf("%d-%d",key,count);
+		}
+		else{
+			printf("Element not found ");
+		}
+		count=0;
+	}
+}
