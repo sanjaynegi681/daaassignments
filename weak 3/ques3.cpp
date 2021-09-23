a8b895db2bd986da62995ed889d048af1df84d2a
sanjaynegi681 <74060688+sanjaynegi681@users.noreply.github.com>
2021-09-23 23:20:00 +0530

    Update ques3.cpp

--- weak 3/ques3.cpp
+++ weak 3/ques3.cpp
@@ -1,37 +1,58 @@
+   
 #include<iostream>
 #include<fstream>
 using namespace std;
+void swap(int *a,int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int partition(int arr[],int low,int high){
+    int pivot = arr[high];
+    int i = low-1;
+    for(int j=low;j<high;j++){
+        if(arr[j]<=pivot){
+            i++;
+            swap(&arr[i],&arr[j]);
+        }
+    }
+    swap(&arr[i+1],&arr[high]);
+    return i+1;
+}
+void quickSort(int arr[],int low,int high){
+    if(low<high){
+    int pi = partition(arr,low,high);
+    quickSort(arr,low,pi-1);
+    quickSort(arr,pi+1,high);
+    }
+}
+
 int main(){
     ifstream in;
-    ofstream out;
     in.open("input.txt");
+    ofstream out;
     out.open("output.txt");
-    int n,t;
-    in >> t;
+    int t,n;
     int arr[100];
+    in >> t;
     while(t){
         in >> n;
         for(int i=0;i<n;i++){
             in >> arr[i];
         }
-        int mid=0;
-        int comparison=0,swaps=0;
+        quickSort(arr,0,n-1);
+        int flag=0;
         for(int i=0;i<n-1;i++){
-            mid=i;
-            for(int j=i+1;j<n;j++){
-                if(arr[j]<arr[mid]){
-                    mid=j;
-                }
-                comparison++;
+            if(arr[i]==arr[i+1]){
+                flag=1;
+                break;
             }
-            swap(arr[mid],arr[i]);
-            swaps++;
         }
-        for(int i=0;i<n;i++)
-            out << arr[i] << " ";
-            out << endl;
-        out << "Comparisons = " << comparison << endl;
-        out << "Swaps = " << swaps << endl;
-        t--;
+        if(flag==1)
+        out << "YES" << endl;
+        else
+        out << "NO" << endl;
+    t--;
     }
+
 }
